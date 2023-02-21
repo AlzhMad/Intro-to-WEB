559eccd5cde014e464cbbe80b324a702aa5720ce
AlzhMad <dzheremy@gmail.com>
2023-02-21 12:32:06 +0600

    task3

---
+++ Chapter6/task3/script.js
@@ -0,0 +1,16 @@
+let n = parseInt(prompt('Enter the number of elements'));
+let arr = [];
+
+while (n>0) {
+    let i = parseInt(prompt('Enter an element'));
+    arr.push(i);
+    n--;  
+}
+
+let sum = 0;
+
+for(let i=0;i<arr.length;i++){
+    sum = sum + arr[i];
+}
+
+alert(sum/arr.length);
